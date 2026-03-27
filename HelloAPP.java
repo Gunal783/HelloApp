0e095bd8fad5e0f8a945b9d3febda34c3c7b331a
Gunal783 <gs8903@srmist.edu.in>
2026-03-27 21:43:29 +0530

    adding UC4

--- HelloAPP.java
+++ HelloAPP.java
@@ -1,11 +1,18 @@
-
-public class HelloAPP {
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        }
-    }
-}
+public class HelloAPP{
+	public static void main (String [] args){
+		if(args.length==0){
+			System.out.println("Hello World!");
+		}
+		else{
+			System.out.print("Hello"+"");
+			for (int i=0;i<args.length;i++){
+				System.out.print(args[i]);
+				if(i>args.length){
+					System.out.print(",");
+				}
+				
+			}
+			System.out.println("!");
+		}
+	}
+}	
